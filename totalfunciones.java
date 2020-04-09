9853899351bc160b275e44029d1fbc2fdc1f2045
titomelen <javiermelendez744@gmail.com>
2020-04-09 20:29:03 +0200

    #3 Subo la funcion2

--- totalfunciones.java
+++ totalfunciones.java
@@ -25,4 +25,50 @@ public class totalfunciones {
 		}
 		return resp;
 	}
+
+	public static String funcion2(String cadena, String cadena2) {
+
+		String resp = null;
+		int contadorVocal = 0;
+		int contadorVocal2 = 0;
+		int contadorConso = 0;
+		int contadorConso2 = 0;
+
+
+
+		for (int i = 0; i < cadena.length(); i++) {
+			if ((cadena.charAt(i) == 'a') || (cadena.charAt(i) == 'e') || (cadena.charAt(i) == 'i')
+					|| (cadena.charAt(i) == 'o') || (cadena.charAt(i) == 'u')) {
+
+				contadorVocal++;
+			}
+			contadorConso = cadena.length() - contadorVocal;
+		}
+
+		for (int i = 0; i < cadena2.length(); i++) {
+			if ((cadena2.charAt(i) == 'a') || (cadena2.charAt(i) == 'e') || (cadena2.charAt(i) == 'i')
+					|| (cadena2.charAt(i) == 'o') || (cadena2.charAt(i) == 'u')) {
+
+				contadorVocal2++;
+			}
+			contadorConso2 = cadena2.length() - contadorVocal2;
+		}
+
+		if (contadorConso > contadorConso2) {
+
+			resp = cadena;
+
+		}
+		else if(contadorConso < contadorConso2) {
+
+			resp = cadena2;
+
+		}
+		else if(contadorConso == contadorConso2) {
+
+			resp=null;
+
+		}
+		return resp;
+	}
 }
