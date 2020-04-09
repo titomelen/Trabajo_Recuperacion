9504fab060624368096801e286c77322c389ebf3
titomelen <javiermelendez744@gmail.com>
2020-04-09 20:49:31 +0200

    #9 Subo la funcion5

--- totalfunciones.java
+++ totalfunciones.java
@@ -117,4 +117,23 @@ public class totalfunciones {
 		return resp;
 	}
 
+	public static Integer funcion5(String simbolo, int num1, int num2) {
+
+		int resp = 0;
+
+		if (simbolo == "+") {
+			resp = num1 + num2;
+		}
+		else if(simbolo == "-") {
+			resp = num1 - num2;
+		}
+		else if(simbolo == "*") {
+			resp = num1 * num2;
+		}
+		else {
+			resp = 0;
+		}
+		return resp;
+	}
+
 }
