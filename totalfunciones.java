5aac597e264ac7e3b43976dc0544fda8abe6d2d6
titomelen <javiermelendez744@gmail.com>
2020-04-09 20:44:43 +0200

    #7 Subo la funcion4

--- totalfunciones.java
+++ totalfunciones.java
@@ -86,4 +86,35 @@ public class totalfunciones {
 		return resp;
 	}
 
+	public static String funcion4(int[] notas) {
+
+		int sumaNotas = 0;
+		int mediaNotas = 0;
+		String resp = null;
+
+		for (int i = 0; i < notas.length; i++) {
+			sumaNotas = sumaNotas + notas[i];
+		}
+
+		mediaNotas = sumaNotas / notas.length;
+
+		if (mediaNotas < 5 && mediaNotas > 0) {
+			resp = "Insuficiente";
+		}
+		else if(mediaNotas >= 5 && mediaNotas < 6) {
+			resp = "Suficiente";
+		}
+		else if(mediaNotas >= 6 && mediaNotas < 7) {
+			resp = "Bien";
+		}
+		else if(mediaNotas >= 7 && mediaNotas < 9) {
+			resp = "Notable";
+		}
+		else if(mediaNotas >= 9 && mediaNotas ==10) {
+			resp = "Sobresaliente";
+		}
+
+		return resp;
+	}
+
 }
