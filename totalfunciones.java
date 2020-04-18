e7c7245522a0cc8168f5c06c2c6784510ebee77b
titomelen <javiermelendez744@gmail.com>
2020-04-18 21:36:39 +0200

    #11 Subo la funcion1 bien

--- totalfunciones.java
+++ totalfunciones.java
@@ -9,16 +9,21 @@ public class totalfunciones {
 
 		for (int i = 0; i < cadena.length(); i++) {
 			if ((cadena.charAt(i) == 'a') || (cadena.charAt(i) == 'e') || (cadena.charAt(i) == 'i')
-					|| (cadena.charAt(i) == 'o') || (cadena.charAt(i) == 'u')) {
-
+					|| (cadena.charAt(i) == 'o') || (cadena.charAt(i) == 'u') || (cadena.charAt(i) == 'A')
+					|| (cadena.charAt(i) == 'E') || (cadena.charAt(i) == 'I') || (cadena.charAt(i) == 'O')
+					|| (cadena.charAt(i) == 'U') || (cadena.charAt(i) == 'á')|| (cadena.charAt(i) == 'é')
+					|| (cadena.charAt(i) == 'í')|| (cadena.charAt(i) == 'ó')|| (cadena.charAt(i) == 'ú')
+					|| (cadena.charAt(i) == 'Á')|| (cadena.charAt(i) == 'É')|| (cadena.charAt(i) == 'Í')
+					|| (cadena.charAt(i) == 'Ó')|| (cadena.charAt(i) == 'Ú')) {
+				
 				contador++;
 
 				if (contador >= edad) {
-
+					
 					 resp = true;
-
+					
 				}else if(contador < edad){
-
+					
 					 resp = false;
 				}
 			}
