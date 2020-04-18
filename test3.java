51a374eefda8a9ef46955061b1c73bdb4b27f833
titomelen <javiermelendez744@gmail.com>
2020-04-18 21:58:38 +0200

    #Subo el test de la funcion3 bien

--- test3.java
+++ test3.java
@@ -55,42 +55,49 @@ class test3 {
 	@DisplayName(value = "Prueba de caja negra con valor limite superior +1")
 	@Test
 	void test3funcion3() {
-		assertEquals(22, totalfunciones.funcion3(new int[] {22,23,24,25,26}));
+		assertEquals(0, totalfunciones.funcion3(new int[] {23,81,72,915,874}));
 	}
 	
 	@DisplayName(value = "Prueba de caja negra con valor limite inferior -1")
 	@Test
 	void test4funcion3() {
-		assertEquals(11, totalfunciones.funcion3(new int[] {7,8,9,10,11}));
+		assertEquals(0, totalfunciones.funcion3(new int[] {-797,-865,-947,-1025,10}));
 	}
+	
+	@DisplayName(value = "Prueba de caja negra con valor nulo")
+	@Test
+	void test5funcion3() {
+		assertEquals(null, totalfunciones.funcion3(null));
+	}
+	
 	
 	//PRUEBAS DE CAJA BLANCA
 	
 	@DisplayName(value = "Prueba de caja blanca con cobertura de decisiones")
 	@Test
 	//Vamos a decidir que el valor que salga sea nulo, por lo que vamos a poner valores que esten por encima y por debajo de los limites.
-	void test5funcion3() {
+	void test6funcion3() {
 		assertEquals(0, totalfunciones.funcion3(new int[] {9,10,23,24,25}));
 	}
 	
 	@DisplayName(value = "Prueba de caja blanca con cobertura de condicion")
 	@Test
 	//Vamos a hacer que ciertos valores no entren en el rango por lo cual saldrá el que si se encuentre dentro del mismo, sea pues 19.
-	void test6funcion3() {
+	void test7funcion3() {
 		assertEquals(19, totalfunciones.funcion3(new int[] {5,6,7,19,77}));
 	}
 	
 	@DisplayName(value = "Prueba de caja blanca con cobertura de condicion")
 	@Test
 	//Vamos a hacer que los valores 19 y 22 se sumen, pero se ignoren los demás valores que no entren en el rango.
-	void test7funcion3() {
+	void test8funcion3() {
 		assertEquals(41, totalfunciones.funcion3(new int[] {5,6,19,22,123}));
 	}
 	
 	@DisplayName(value = "Prueba de caja blanca con cobertura de condicion")
 	@Test
 	//Vamos a hacer que los valores sean negativos, por lo que no entraran en el rango y dará 0.
-	void test8funcion3() {
+	void test9funcion3() {
 		assertEquals(0, totalfunciones.funcion3(new int[] {-5,-6,-7,-20,-77}));
 	}
 
