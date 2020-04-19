b8076be1a0c17b467fd8b373b8811139b9a9f30b
titomelen <javiermelendez744@gmail.com>
2020-04-19 12:04:55 +0200

    #19 Subo bien la funcion5

--- totalfunciones.java
+++ totalfunciones.java
@@ -1,126 +1,10 @@
 package Proyecto;
 
 public class totalfunciones {
-	public static Boolean funcion1(String cadena) {
-
-		Boolean resp = null;
-		int edad = 22;
-		int contador = 0;
-
-		for (int i = 0; i < cadena.length(); i++) {
-			if ((cadena.charAt(i) == 'a') || (cadena.charAt(i) == 'e') || (cadena.charAt(i) == 'i')
-					|| (cadena.charAt(i) == 'o') || (cadena.charAt(i) == 'u')) {
-
-				contador++;
-
-				if (contador >= edad) {
-
-					 resp = true;
-
-				}else if(contador < edad){
-
-					 resp = false;
-				}
-			}
-		}
-		return resp;
-	}
-
-	public static String funcion2(String cadena, String cadena2) {
-
-		String resp = null;
-		int contadorVocal = 0;
-		int contadorVocal2 = 0;
-		int contadorConso = 0;
-		int contadorConso2 = 0;
-
-
-
-		for (int i = 0; i < cadena.length(); i++) {
-			if ((cadena.charAt(i) == 'a') || (cadena.charAt(i) == 'e') || (cadena.charAt(i) == 'i')
-					|| (cadena.charAt(i) == 'o') || (cadena.charAt(i) == 'u')) {
-
-				contadorVocal++;
-			}
-			contadorConso = cadena.length() - contadorVocal;
-		}
-
-		for (int i = 0; i < cadena2.length(); i++) {
-			if ((cadena2.charAt(i) == 'a') || (cadena2.charAt(i) == 'e') || (cadena2.charAt(i) == 'i')
-					|| (cadena2.charAt(i) == 'o') || (cadena2.charAt(i) == 'u')) {
-
-				contadorVocal2++;
-			}
-			contadorConso2 = cadena2.length() - contadorVocal2;
-		}
-
-		if (contadorConso > contadorConso2) {
-
-			resp = cadena;
-
-		}
-		else if(contadorConso < contadorConso2) {
-
-			resp = cadena2;
-
-		}
-		else if(contadorConso == contadorConso2) {
-
-			resp=null;
-
-		}
-		return resp;
-	}
-
-	public static Integer funcion3(int[] listaNumeros) {
-
-		int edad = 22;
-		int mitadEdad = 11;
-		int resp = 0;
-
-		for (int i = 0; i < listaNumeros.length; i++) {
-			if (listaNumeros[i] <= edad && listaNumeros[i] >= mitadEdad) {
-				resp= resp + listaNumeros[i];
-			}
-		}
-		return resp;
-	}
-
-	public static String funcion4(int[] notas) {
-
-		int sumaNotas = 0;
-		int mediaNotas = 0;
-		String resp = null;
-
-		for (int i = 0; i < notas.length; i++) {
-			sumaNotas = sumaNotas + notas[i];
-		}
-
-		mediaNotas = sumaNotas / notas.length;
-
-		if (mediaNotas < 5 && mediaNotas > 0) {
-			resp = "Insuficiente";
-		}
-		else if(mediaNotas >= 5 && mediaNotas < 6) {
-			resp = "Suficiente";
-		}
-		else if(mediaNotas >= 6 && mediaNotas < 7) {
-			resp = "Bien";
-		}
-		else if(mediaNotas >= 7 && mediaNotas < 9) {
-			resp = "Notable";
-		}
-		else if(mediaNotas >= 9 && mediaNotas ==10) {
-			resp = "Sobresaliente";
-		}
-
-		return resp;
-	}
-
 	public static Integer funcion5(String simbolo, int num1, int num2) {
-
+		
 		int resp = 0;
-
+		
 		if (simbolo == "+") {
 			resp = num1 + num2;
 		}
@@ -130,10 +14,12 @@ public class totalfunciones {
 		else if(simbolo == "*") {
 			resp = num1 * num2;
 		}
+		else if(simbolo == "") {
+			return null;
+		}
 		else {
 			resp = 0;
 		}
 		return resp;
 	}
-
 }
